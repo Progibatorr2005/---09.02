04dffb93ece66fc657eb3f32bb9dacb841a27b63
Ilya Bagay <ilyabaglay20@gmail.com>
2025-01-14 10:57:43 +0900

    3

---
+++ 3.tsx
@@ -0,0 +1,13 @@
+function getHiddenCard (num: string, a? : number) {
+    let n = ''
+    let v = 
+
+    return
+}
+
+
+
+getHiddenCard('1234567812345678', 2) // "**5678"
+getHiddenCard('1234567812345678', 3) // "***5678"
+getHiddenCard('1234567812345678')    // "****5678"
+getHiddenCard('2034399002121100', 1) // "*1100"
